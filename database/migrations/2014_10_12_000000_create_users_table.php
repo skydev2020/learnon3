8bcf6fdbfc566d972ac927325e276a85090d2874
Sky Dev <skydev2020@outlook.com>
2020-02-15 10:54:34 -0500

    create register/login

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('ip')->nullable();
+            $table->integer('approved')->nullable();
+            $table->integer('status')->nullable();
+            $table->string('last_login')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
