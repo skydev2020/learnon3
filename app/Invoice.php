93ee675b6b36a09f3f6bd689126b4aae95a01005
Soft Actor <champion00531@yahoo.com>
2020-03-25 17:42:17 +0800

    Update Invoices

--- app/Invoice.php
+++ app/Invoice.php
@@ -9,4 +9,8 @@ class Invoice extends Model
     public function users() {
         return $this->belongsTo('App\User', 'user_id');
     }
+
+    public function orders() {
+        return $this->hasMany('App\Order', 'invoice_pk');
+    }
 }
