aa7dab826d7d00422e9835aa2cde6cbbdaf4c34b
Soft Actor <champion00531@yahoo.com>
2020-04-09 00:02:23 -0600

    update grades

--- resources/views/admin/grades/index.blade.php
+++ resources/views/admin/grades/index.blade.php
@@ -5,7 +5,9 @@
     <div class="row justify-content-center">
         <div class="col-12">
             <div class="card">
-                <div class="card-header">Grades</div>
+                <div class="card-header">
+                    <i class="fa fa-info-circle" style="font-size:24px"> Grades</i>
+                </div>
                 <div class="card-body">
                     <form action="{{ route('admin.grades.create') }}">
                         <div class="form-group row mb-0">
