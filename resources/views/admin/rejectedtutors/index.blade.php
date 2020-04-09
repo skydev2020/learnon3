1a96500d706715a87df731bdf7eb256b66b25bb7
Soft Actor <champion00531@yahoo.com>
2020-04-09 16:11:20 -0600

    update rejected tutors

--- resources/views/admin/rejectedtutors/index.blade.php
+++ resources/views/admin/rejectedtutors/index.blade.php
@@ -5,7 +5,9 @@
     <div class="row justify-content-center">
         <div class="col-12">
             <div class="card">
-                <div class="card-header user font-weight-bold">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Rejected Tutors</div>
+                <div class="card-header">
+                    <i class="fas fa-user-tie" style="font-size:24px"> Rejected Tutors</i>
+                </div>
 
                 <div class="card-body">
                     <form method="GET" action="{{ route('admin.rejectedtutors.index') }}">
