42c99bded9e9e4dba571742497c1b9bcccffd339
skydev2020 <skydev2020@outlook.com>
2020-04-23 18:50:14 -0400

    Add footere

---
+++ resources/views/partials/footer.php
@@ -0,0 +1,3 @@
+<div class="container-fluid d-flex text-white align-items-center justify-content-center" id="footer">    
+    <?php echo ' &copy; 2009-' . date('Y') . ' All Rights Reserved.';?>
+</div>
